824aa6f1116f953b1e45343a4956e18d7487b4df
0xHy0kR1 <helloweenhell666@gmail.com>
2023-08-11 23:16:10 +0530

    Added search box and carl image url and modified carousel

---
+++ src/components/Card.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+export default function Card() {
+  return (
+    <div>
+    <div class="card mt-3" style={{"maxHeight":"360px","width": "18rem"}}>
+      <img src="https://www.themealdb.com/images/media/meals/lpd4wy1614347943.jpg" class="card-img-top" style={{"maxHeight":"180px","width": "18rem"}} alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <div className="container w-100">
+          {/* The below javascript is just for quantity representation of the items */}
+          <select className="m-2 bg-success rounded">
+            {Array.from(Array(6),(e,i) => {
+                return(
+                  <option key={i+1} value={i+1}>{i+1}</option>
+                )
+            })}
+          </select>
+
+          {/* The below javascript is just for whether you want half, full, or medium */}
+          <select className="m-2 bg-success rounded">
+              <option value="half">Half</option>
+              <option value="full">Full</option>
+          </select>
+
+          {/* General text as an information */}
+          <div className="d-inline h-100 fs-5">
+            Total Price
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
---
+++ src/components/Carousal.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default function Carousal() {
+    return (
+        <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel" style={{objectFit: "cover !important"}}>
+            <div className="carousel-inner">
+                <div className="carousel-caption" style={{"z-index":"10"}}>
+                    <form className="d-flex">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                        <button className="btn btn-outline-success text-white" type="submit">Search</button>
+                    </form>
+                </div>
+                <div className="carousel-item active" style={{objectFit: "cover !important"}}>
+                    <img src="https://source.unsplash.com/random/900×700/?burger" style={{ filter: "brightness(30%)", height: "600px"}} className="d-block w-100" alt="..."/>
+                </div>
+                <div className="carousel-item" style={{objectFit: "cover !important"}}>
+                    <img src="https://source.unsplash.com/random/900×700/?chicken" style={{ filter: "brightness(30%)", height: "600px"}} className="d-block w-100" alt="..."/>
+                </div>
+                <div className="carousel-item" style={{objectFit: "cover !important"}}>
+                    <img src="https://source.unsplash.com/random/900×700/?pizza" style={{ filter: "brightness(30%)", height: "600px"}} className="d-block w-100" alt="..."/>
+                </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
+        </div>
+    )
+}
